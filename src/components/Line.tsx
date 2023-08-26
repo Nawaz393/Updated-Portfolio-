ae134ba6401580276b60e209490d945ee11f7948
mohdnawaz6393@gmail.com <mohdnawaz6393@gmail.com>
2023-08-26 15:57:51 +0500

    added google search console tag

--- src/components/Line.tsx
+++ src/components/Line.tsx
@@ -1,15 +1,10 @@
 import  { ReactElement } from 'react'
 
-
-
 interface Prop {
-
   text: string
-
 }
 export const Line = ({ text }: Prop): ReactElement => {
   return (
-
     <div className='h-20  bcol mx-5  rounded-tl-full rounded-br-full grid place-items-center ' >
 
       <h2 className=' text-white font-bold md:text-4xl text-2xl font-script  capitalize'>{text} </h2>
